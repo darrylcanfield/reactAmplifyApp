c6661166586c210c68eff195a2b4651d69b0c324
darrylcanfield <112822607+darrylcanfield@users.noreply.github.com>
2025-01-06 14:21:25 -0500

    error fix installed

---
+++ src/navbar.tsx
@@ -0,0 +1,41 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">RealGrappling</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="Throws & Takedows" id="basic-nav-dropdown">
+              <NavDropdown.Item href="standupCollarSleeve">Standup: Collar & Sleeve</NavDropdown.Item>
+              <NavDropdown.Item href="standupCrossLapel">Standup: Cross Lapel</NavDropdown.Item>
+              <NavDropdown.Item href="standupLapelAndElbow">Standup: Lapel & Elbow</NavDropdown.Item>
+              <NavDropdown.Divider />
+            </NavDropdown>
+            <NavDropdown title="Guard Passing" id="basic-nav-dropdown">
+            <NavDropdown.Item href="passingOpenGuard">Pass Open Guard</NavDropdown.Item>
+              <NavDropdown.Item href="passingHalfGuard">Pass Half Guard</NavDropdown.Item>
+              <NavDropdown.Item href="passingSingleLegX">Pass Single Leg X</NavDropdown.Item>
+              <NavDropdown.Divider />
+            </NavDropdown>
+            <NavDropdown title="Guard Attacks / Retention" id="basic-nav-dropdown">
+              <NavDropdown.Item href="attackingOpenGuard">Attacking from Open Guard</NavDropdown.Item>
+              <NavDropdown.Item href="attackingHalfGuard">Attacking from Half Guard</NavDropdown.Item>
+              <NavDropdown.Item href="attackingSingleLegX">Attacking from Single Leg X</NavDropdown.Item>
+              <NavDropdown.Divider />
+            </NavDropdown>
+            <Nav.Link href="myList">My List</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
---
+++ src/routes/myList.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import type { Schema } from "../../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import { Link, Route, Routes } from "react-router";
+import { Container, Row, Col } from "react-bootstrap";
+
+const client = generateClient<Schema>();
+
+function MyList () {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const { signOut } = useAuthenticator();
+
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }, []);
+
+  function createTodo() {
+    client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
+ 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col sm={8}><h4>Learning List: (Click to add or remove)</h4></Col>
+        <Col sm={4}><button onClick={createTodo}>+ new</button></Col>
+      </Row>
+      
+      <ul>
+        {todos.map((todo) => (
+          <li 
+            onClick={() => deleteTodo(todo.id)}
+            key={todo.id}>
+            {todo.content}
+          </li>
+        ))}
+      </ul>
+    </Container>
+  );
+};
+
+export default MyList; 
