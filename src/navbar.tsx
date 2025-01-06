ec47c322ba0f9ffeb28b30ef89a5938fc21557c8
darrylcanfield <112822607+darrylcanfield@users.noreply.github.com>
2025-01-06 15:57:25 -0500

    navbar fix

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -12,25 +12,25 @@ function NavBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <NavDropdown title="Throws & Takedows" id="basic-nav-dropdown">
-              <NavDropdown.Item href="standupCollarSleeve">Standup: Collar & Sleeve</NavDropdown.Item>
-              <NavDropdown.Item href="standupCrossLapel">Standup: Cross Lapel</NavDropdown.Item>
-              <NavDropdown.Item href="standupLapelAndElbow">Standup: Lapel & Elbow</NavDropdown.Item>
+              <NavDropdown.Item href="/standupCollarSleeve">Standup: Collar & Sleeve</NavDropdown.Item>
+              <NavDropdown.Item href="/standupCrossLapel">Standup: Cross Lapel</NavDropdown.Item>
+              <NavDropdown.Item href="/standupLapelAndElbow">Standup: Lapel & Elbow</NavDropdown.Item>
               <NavDropdown.Divider />
             </NavDropdown>
             <NavDropdown title="Guard Passing" id="basic-nav-dropdown">
-            <NavDropdown.Item href="passingOpenGuard">Pass Open Guard</NavDropdown.Item>
-              <NavDropdown.Item href="passingHalfGuard">Pass Half Guard</NavDropdown.Item>
-              <NavDropdown.Item href="passingSingleLegX">Pass Single Leg X</NavDropdown.Item>
+            <NavDropdown.Item href="/passingOpenGuard">Pass Open Guard</NavDropdown.Item>
+              <NavDropdown.Item href="/passingHalfGuard">Pass Half Guard</NavDropdown.Item>
+              <NavDropdown.Item href="/passingSingleLegX">Pass Single Leg X</NavDropdown.Item>
               <NavDropdown.Divider />
             </NavDropdown>
             <NavDropdown title="Guard Attacks / Retention" id="basic-nav-dropdown">
-              <NavDropdown.Item href="attackingOpenGuard">Attacking from Open Guard</NavDropdown.Item>
-              <NavDropdown.Item href="attackingHalfGuard">Attacking from Half Guard</NavDropdown.Item>
-              <NavDropdown.Item href="attackingSingleLegX">Attacking from Single Leg X</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingOpenGuard">Attacking from Open Guard</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingHalfGuard">Attacking from Half Guard</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingSingleLegX">Attacking from Single Leg X</NavDropdown.Item>
               <NavDropdown.Divider />
             </NavDropdown>
-            <Nav.Link href="myList">My List</Nav.Link>
-            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="/myList">My List</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
