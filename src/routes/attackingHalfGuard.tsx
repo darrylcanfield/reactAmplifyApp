e4dbbaecf482f826609c86ea7a46fea8b1c2675d
darrylcanfield <112822607+darrylcanfield@users.noreply.github.com>
2025-01-11 08:14:08 -0500

    secondary buttons and image

--- src/routes/attackingHalfGuard.tsx
+++ src/routes/attackingHalfGuard.tsx
@@ -15,6 +15,10 @@ interface DataItem {
   why: string;
   name: string;
   description: string;
+  img: string;
+  b1: string;
+  b2: string;
+  b3: string;
 }
 
 const AttackingHalfGuard = () => {
@@ -28,7 +32,7 @@ const AttackingHalfGuard = () => {
     client.models.Todo.create({ content });
 
     // Set a success message for a new todo
-    setNotification(`New Todo added: "${content}"`);
+    setNotification(`Favorite added: "${content}"`);
 
     // Clear the notification after 3 seconds
     setTimeout(() => setNotification(null), 3000);
@@ -55,11 +59,11 @@ const AttackingHalfGuard = () => {
           {notification}
         </div>
       )}
-
-      <Row>
+    
+    <Row>
         <Col>
           <Card>
-            <Card.Img variant="top" src="https://i.ytimg.com/vi/Q_WjpBd_Yz8/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDjCwWPPylEfmaS7PMJpHdibbaksg" />
+            <Card.Img className="realimg" variant="top" src="https://i.ytimg.com/vi/Q_WjpBd_Yz8/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDjCwWPPylEfmaS7PMJpHdibbaksg" />
             <Card.Body>
               <Card.Title>Attacking From Half Guard</Card.Title>
               <Card.Text>
@@ -69,11 +73,15 @@ const AttackingHalfGuard = () => {
               </Card.Text>
 
               {/* Buttons to show data */}
+              <Row>
               {data.map((item) => (
+                <Col>
                 <Button className="reactionButton" variant="warning" key={item.id} onClick={() => handleButtonClick(item)}>
                   Reaction: {item.why}
                 </Button>
+                </Col>
               ))}
+              </Row>
               
               {/* Dynamically rendered component */}
               <div style={{ marginTop: "20px" }}>
@@ -85,19 +93,45 @@ const AttackingHalfGuard = () => {
                       borderRadius: "5px",
                     }}
                   >
-                    <h2>{activeItem.name}</h2>
-                    <p>{activeItem.description}</p>
-                    <Button
-                      variant="primary"
-                      onClick={() => {
-                        if (activeItem) {
-                          createTodo(activeItem.name);
-                        } else {
-                          alert("No active item selected");
-                        }
-                      }}
-                    >+Favorite
-                    </Button>
+                  <Row>
+                    <Col><img  className="realimg" src={activeItem.img} /></Col>
+                    <Col><h2>{activeItem.name}</h2></Col>
+                    <Col>
+                      <Button
+                        variant="primary"
+                        onClick={() => {
+                          if (activeItem) {
+                            createTodo(activeItem.name);
+                          } else {
+                            alert("No active item selected");
+                          }
+                        }}
+                      >+Favorite
+                      </Button>
+                    </Col>
+                  </Row>
+
+                  <Row>
+                    <Col> <p>{activeItem.description}</p></Col>
+                  </Row>
+
+                    {/* Buttons to show data */}
+              <Row>
+                <Col>
+                <Button className="reactionButton" variant="warning">
+                   {activeItem.b1}
+                </Button>
+                </Col>
+                <Col>
+                <Button className="reactionButton" variant="warning">
+                   {activeItem.b2}
+                </Button>
+                </Col>
+              </Row>
+
+                    
+                   
+                    
                   </div>
                 )}
               </div>
