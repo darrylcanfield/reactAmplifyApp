5533eb4c08297e34d4ad8f518eb88cce000834e2
darrylcanfield <112822607+darrylcanfield@users.noreply.github.com>
2025-02-21 08:52:27 -0500

    positions added

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import Transitions from "./routes/transitions.tsx";
 import About from "./routes/about.tsx";
 import Home from "./routes/home.tsx";
 import MyList from "./routes/myList.tsx";
+import { Container } from "react-bootstrap";
+import Navbar from "./navbar.tsx";
+
 import StandupUnderhook from "./routes/standupUnderhook.tsx";
 import StandupOverhook from "./routes/standupOverhook.tsx";
-import StandupLapelAndElbow from "./routes/standupLapelAndElbow.tsx";
+import Standup2on1WristControl from "./routes/standup2on1WristControl.tsx";
+import StandupCollarTie from "./routes/standupCollarTie.tsx";
+import StandupRussian from "./routes/standupRussian.tsx";
+import StandupWrstControl from "./routes/standupWristControl.tsx";
+import StandupNoGrips from "./routes/standupNoGrips.tsx";
+
 import PassingHalfGuardStanding from "./routes/passingHalfGuardStanding.tsx";
 import PassingHalfGuardKneeling from "./routes/passingHalfGuardKneeling.tsx";
 import PassingOpenGuardSupine from "./routes/passingOpenGuardSupine.tsx";
@@ -14,10 +22,31 @@ import PassingHalfButterfly from "./routes/passingHalfButterfly.tsx";
 import PassingButterflyGuard from "./routes/passingButterflyGuard.tsx";
 import PassingHeadquarters from "./routes/passingHeadquarters.tsx";
 import PassingSingleLegX from "./routes/passingSingleLegX";
+
+import Attacking5050 from "./routes/attacking5050.tsx";
+import AttackingButterflyGuard from "./routes/attackingButterFlyGuard.tsx";
+import AttackingChoiGuard from "./routes/attackingChoiGuard.tsx";
+import AttackingClosedGuard from "./routes/attackingClosedGuard.tsx";
+import AttackingCrabRide from "./routes/attackingCrabRide.tsx";
+import AttackingDeLaRiva from "./routes/attackingDeLaRiva.tsx";
+import AttackingHalfButterfly from "./routes/attackingHalfButterfly.tsx";
 import AttackingHalfGuard from "./routes/attackingHalfGuard.tsx";
+import AttackingInsideReap from "./routes/attackingInsideReap.tsx";
+import AttackingInvertedGuard from "./routes/attackingInvertedGuard.tsx";
+import AttackingKGuard from "./routes/attackingKGuard.tsx";
+import AttackingLegDrag from "./routes/attackingLegDrag.tsx";
+import AttackingMount from "./routes/attackingMount.tsx";
+import AttackingNorthSouth from "./routes/attackingNorthSouth.tsx";
+import AttackingOctopusGuard from "./routes/attackingOctopusGuard.tsx";
+import AttackingOutsideAshi from "./routes/attackingOutsideAshi.tsx";
+import AttackingOutsideReap from "./routes/attackingOutsideReap.tsx";
+import AttackingReverseDLR from "./routes/attackingReverseDLR.tsx";
+import AttackingReverseXGuard from "./routes/attackingReverseXGuard.tsx";
+import AttackingShinToShin from "./routes/attackingShinToShin.tsx";
+import AttackingSideControl from "./routes/attackingSideControl.tsx";
 import AttackingSingleLegX from "./routes/attackingSingleLegX.tsx";
-import { Container } from "react-bootstrap";
-import Navbar from "./navbar.tsx";
+import AttackingTurtle from "./routes/AttackingTurtle.tsx";
+import AttackingBack from "./routes/AttackingBack.tsx";
 
 function App() {
   return (
@@ -28,9 +57,40 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/transitions' element={<Transitions />} />
+
+        <Route path='/attacking5050' element={<Attacking5050 />} />
+        <Route path='/attackingButterflyGuard' element={<AttackingButterflyGuard/>} />
+        <Route path='/attackingChoiGuard' element={<AttackingChoiGuard/>} />
+        <Route path='/attackingClosedGuard' element={<AttackingSingleLegX/>} />
+        <Route path='/attackingCrabRide' element={<AttackingCrabRide/>} />
+        <Route path='/attackingDeLaRiva' element={<AttackingDeLaRiva/>} />
+        <Route path='/attackingHalfButterfly' element={<AttackingHalfButterfly/>} />
+        <Route path='/attackingHalfGuard' element={<AttackingHalfGuard/>} />
+        <Route path='/attackingInsideReap' element={<AttackingInsideReap/>} />
+        <Route path='/attackingInvertedGuard' element={<AttackingInvertedGuard/>} />
+        <Route path='/attackingKGuard' element={<AttackingKGuard/>} />
+        <Route path='/attackingLegDrag' element={<AttackingLegDrag/>} />
+        <Route path='/attackingMount' element={<AttackingMount/>} />
+        <Route path='/attackingOctopusGuard' element={<AttackingOctopusGuard/>} />
+        <Route path='/attackingNorthSouth' element={<AttackingNorthSouth/>} />
+        <Route path='/attackingOutsideAshi' element={<AttackingOutsideAshi/>} />
+        <Route path='/attackingOutsideReap' element={<AttackingOutsideReap/>} />
+        <Route path='/attackingReverseDLR' element={<AttackingReverseDLR/>} />
+        <Route path='/attackingReverseXGuard' element={<AttackingReverseXGuard/>} />
+        <Route path='/attackingShinToShin' element={<AttackingShinToShin/>} />
+        <Route path='/attackingSideControl' element={<AttackingSideControl/>} />
+        <Route path='/attackingSingleLegX' element={<AttackingSingleLegX/>} />
+        <Route path='/attackingTurtle' element={<AttackingTurtle/>} />
+        <Route path='/attackingBack' element={<AttackingBack/>} />
+
         <Route path='/standupUnderhook' element={<StandupUnderhook />} />
         <Route path='/standupOverhook' element={<StandupOverhook/>} />
-        <Route path='/standupLapelAndElbow' element={<StandupLapelAndElbow/>} />
+        <Route path='/standup2on1WristControl' element={<Standup2on1WristControl/>} />
+        <Route path='/standupCollarTie' element={<StandupCollarTie/>} />
+        <Route path='/standupRussian' element={<StandupRussian/>} />
+        <Route path='/standupWristControl' element={<StandupWrstControl/>} />
+        <Route path='/standupNoGrips' element={<StandupNoGrips/>} />
+
         <Route path='/passingOpenGuardSupine' element={<PassingOpenGuardSupine />} />
         <Route path='/passingOpenGuardSeated' element={<PassingOpenGuardSeated />} />
         <Route path='/passingHeadquarters' element={<PassingHeadquarters />} />
@@ -39,8 +99,7 @@ function App() {
         <Route path='/passingButterflyGuard' element={<PassingButterflyGuard />} />
         <Route path='/passingHalfButterfly' element={<PassingHalfButterfly />} />
         <Route path='/passingSingleLegX' element={<PassingSingleLegX />} />
-        <Route path='/attackingHalfGuard' element={<AttackingHalfGuard />} />
-        <Route path='/attackingSingleLegX' element={<AttackingSingleLegX/>} />
+
         <Route path='/myList' element={<MyList />} />
         <Route path='/about' element={<About />} />
       </Routes>
--- src/navbar.tsx
+++ src/navbar.tsx
@@ -16,8 +16,11 @@ function NavBar() {
             <NavDropdown title="Throws & Takedowns" id="basic-nav-dropdown">
               <NavDropdown.Item href="/standupUnderhook">Standup: Underhook</NavDropdown.Item>
               <NavDropdown.Item href="/standupOverhook">Standup: Overhook</NavDropdown.Item>
-              <NavDropdown.Item href="/standupLapelAndElbow">Standup: Lapel & Elbow</NavDropdown.Item>
-              <NavDropdown.Divider />
+              <NavDropdown.Item href="/standupCollarTie">Standup: Collar Tie</NavDropdown.Item>
+              <NavDropdown.Item href="/standupWristControl">Standup: Wrist Control</NavDropdown.Item>
+              <NavDropdown.Item href="/standup2on1WristControl">Standup: 2on1 Wrist Control</NavDropdown.Item>
+              <NavDropdown.Item href="/standupRussian">Standup: Russian</NavDropdown.Item>
+              <NavDropdown.Item href="/standupNoGrips">Standup: No Grips</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Guard Passing" id="basic-nav-dropdown">
               <NavDropdown.Item href="/passingOpenGuardSupine">Pass Open Guard (Supine)</NavDropdown.Item>
@@ -28,16 +31,42 @@ function NavBar() {
               <NavDropdown.Item href="/passingButterflyGuard">Pass Butterfly Guard</NavDropdown.Item>
               <NavDropdown.Item href="/passingHeadquarters">Pass Headquarters</NavDropdown.Item>
               <NavDropdown.Item href="/passingSingleLegX">Pass Single Leg X</NavDropdown.Item>
-              <NavDropdown.Divider />
+            </NavDropdown>
+            <NavDropdown title="Top Attacks" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/attackingSideControl">Side Control</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingTurtle">Turtle</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingBack">Back</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingMount">Mount</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingBack">Back Control</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingNorthSouth">North South</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Guard Attacks" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/attackingHalfGuard">Attacking from Half Guard</NavDropdown.Item>
-              <NavDropdown.Item href="/attackingSingleLegX">Attacking from Single Leg X</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingClosedGuard">Closed Guard</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingShinToShin">Shin to Shin</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingSingleLegX">Single Leg X</NavDropdown.Item>              
+              <NavDropdown.Item href="/attackingHalfGuard">Half Guard</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingButterflyGuard">Butterfly Guard</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingHalfButterfly">Half Butterfly</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingDeLaRiva">DLR</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingChoiGuard">Choi Guard</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingKGuard">K Guard</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingOctopusGuard">Octopus Guard</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingCrabRide">CrabRide</NavDropdown.Item>
               <NavDropdown.Divider />
+              <NavDropdown.Item href="/attackingInsideReap">Inside Reap</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingOutsideAshi">Outside Ashi</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingOutsideReap">Outside Reap</NavDropdown.Item>
+              <NavDropdown.Item href="/attacking5050">50/50</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingReverseXGuard">Reverse X Guard</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingInvertedGuard">Inverted Guard</NavDropdown.Item>
+              <NavDropdown.Item href="/attackingLegDragGuard">Leg Drag Guard</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="/transitions">Transitions</Nav.Link>         
-            <Nav.Link href="/myList">MyList</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
+            <NavDropdown title="Account" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/transitions">Transitions</NavDropdown.Item>
+              <NavDropdown.Item href="/myList">MyList</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/about">About</NavDropdown.Item>
+            </NavDropdown>
             <button onClick={signOut}>Sign out</button>
           </Nav>
         </Navbar.Collapse>
